921d64734aef038295ec337f485f24cad22a1fb3
bicur <159040785+bicur@users.noreply.github.com>
2024-05-27 22:31:17 +0200

    Lade till Johns javascript

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden, .hiddenfromright');
+hiddenElements.forEach((el) => observer.observe(el))
+
+
 window.addEventListener('scroll', function() {
     var parallax = document.querySelector('.parallax');
     var scrollPosition = window.pageYOffset;
